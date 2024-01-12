8c5281c31127ada5a6f8b71f98b262b6606b7e2f
Standalone <coder2k@mail.ru>
2024-01-12 18:22:30 +0300

    ESLINT  + CLEAN INIT PROJECT

---
+++ .eslintrc.js
@@ -0,0 +1,73 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'import/no-cycle': 0,
+    'no-console': 0,
+    'react/prop-types': 0,
+    'linebreak-style': 0,
+    'react/state-in-constructor': 0,
+    'import/prefer-default-export': 0,
+    'max-len': [2, 250],
+    'object-curly-newline': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/alt-text': 0,
+    'jsx-a11y/no-autofocus': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'react/no-array-index-key': 0,
+    'no-param-reassign': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
+    'no-sparse-arrays': 0,
+    'no-array-index-key': 0,
+    camelcase: 0,
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always', // Добавляем пустые строки между группами импортов
+        groups: [
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+        ],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'shared/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+      },
+    ],
+  },
+};
